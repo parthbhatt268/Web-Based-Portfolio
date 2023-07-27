281ebac9e832ee0454fd2f4b8d18da30f0afe6c1
parth <parthbhatt268@gmail.com>
2023-07-27 21:07:46 +0530

    UI changes

--- src/component/master/master.js
+++ src/component/master/master.js
@@ -150,7 +150,7 @@ function Master(props) {
                   backgroundColor: "transparent",
                   color: '#fddc5c',
                   fontWeight: '400',
-                  width: "130px",
+                  width: "100px",
                   margin: "20px 0px 10px 0px"
                 }}>
                 <span>
